6f6b461583aae41c3a35399aa82e2f8f27f4310e
ARFAN <khan.arfan07@gmail.com>
2023-10-24 23:10:32 -0400

    index created

---
+++ user/includes/header_bottom.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+
+
+<div class="header-bottom hidden-sm hidden-xs">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-4">
+
+               
+            </div>
+            <div class="col-lg-9 col-md-8 padding-top-2px">
+
+                <div class="live-info">
+                    <p class="telephone"><i class="fa fa-phone" aria-hidden="true"></i><b class="phone-number"> </b></p>
+                    <p class="working-time">Mon-Fri: 8:30am-7:30pm Work here ARFAN</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ user/index.php
@@ -0,0 +1,105 @@
+<?php
+
+// session_start();
+// include_once ("admin/class/adminback.php");
+// $obj = new adminback();
+
+// $cata_info = $obj-> p_display_catagory();
+// $cataDatas = array();
+// while($data = mysqli_fetch_assoc($cata_info)){
+//     $cataDatas[]=$data;
+// }
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+<?php
+include_once("includes/head.php");
+?>
+
+<body class="biolife-body">
+    <!-- Preloader -->
+
+    <?php
+    include_once("includes/preloader.php");
+    ?>
+
+    <!-- HEADER -->
+    <header id="header" class="header-area style-01 layout-03">
+
+        <?php
+        include_once("includes/header_top.php");
+        ?>
+
+        <?php
+        include_once("includes/header_middle.php");
+        ?>
+
+        <?php
+        include_once("includes/header_bottom.php");
+        ?>
+
+    </header>
+
+    <!-- Page Contain -->
+    <div class="page-contain">
+
+        <!-- Main content -->
+        <div id="main-content" class="main-content">
+
+            <!--Block 01: Main Slide-->
+
+            <?php
+            include_once("includes/slider_main.php")
+            ?>
+
+            <!--Block 02: Banners-->
+
+
+
+            <!--Block 03: Product Tabs-->
+
+
+
+            <!--Block 06: Products-->
+
+            <!--Block 07: Brands-->
+
+        </div>
+    </div>
+
+    <!-- FOOTER -->
+
+    <?php
+    include_once("includes/footer.php");
+    ?>
+
+    <!--Footer For Mobile-->
+    <?php
+    include_once("includes/mobile_footer.php");
+    ?>
+
+    <?php
+    include_once("includes/mobile_global.php")
+    ?>
+
+
+    <!-- Scroll Top Button -->
+    <a class="btn-scroll-top"><i class="biolife-icon icon-left-arrow"></i></a>
+
+    <?php
+    include_once("includes/script.php")
+    ?>
+</body>
+
+</html>
