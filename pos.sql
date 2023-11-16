cbe620d42cb32abaa47a8470fa6e762bd577e5ae
ARFAN <khan.arfan07@gmail.com>
2023-11-16 13:50:01 -0500

    database updated

--- pos.sql
+++ pos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2023 at 01:24 AM
+-- Generation Time: Nov 16, 2023 at 07:49 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -31,20 +31,85 @@ CREATE TABLE `admin` (
   `admin_id` int(10) NOT NULL,
   `admin_name` varchar(255) NOT NULL,
   `admin_email` varchar(255) NOT NULL,
-  `admin_pass` varchar(255) NOT NULL
+  `admin_pass` varchar(255) NOT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `reset_token_expire` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`admin_id`, `admin_name`, `admin_email`, `admin_pass`) VALUES
-(1, 'Arfan', 'tcase018@gmail.com', 'e10adc3949ba59abbe56e057f20f883e'),
-(2, 'test', 'team@gmail.com', 'e10adc3949ba59abbe56e057f20f883e'),
-(3, 'signtest', 'sign@gmail.com', 'e10adc3949ba59abbe56e057f20f883e'),
-(4, 'team18Q', 'team18q@gmail.com', 'e10adc3949ba59abbe56e057f20f883e'),
-(5, '', '', 'd41d8cd98f00b204e9800998ecf8427e'),
-(6, 'jiraq', 'jiraq@gmail.com', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `admin` (`admin_id`, `admin_name`, `admin_email`, `admin_pass`, `reset_token`, `reset_token_expire`) VALUES
+(1, 'Arfan', 'tcase018@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '8ea6ad00a3b723d4cd1ed28fb99558f9', '2023-11-11'),
+(2, 'test', 'team@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', NULL, NULL),
+(3, 'signtest', 'sign@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', NULL, NULL),
+(4, 'team18Q', 'team18q@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', NULL, NULL),
+(5, '', '', 'd41d8cd98f00b204e9800998ecf8427e', NULL, NULL),
+(6, 'jiraq', 'jiraq@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `catagory`
+--
+
+CREATE TABLE `catagory` (
+  `ctg_id` int(11) NOT NULL,
+  `ctg_name` varchar(60) NOT NULL,
+  `ctg_des` varchar(255) NOT NULL,
+  `ctg_status` tinyint(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `catagory`
+--
+
+INSERT INTO `catagory` (`ctg_id`, `ctg_name`, `ctg_des`, `ctg_status`) VALUES
+(2, 'Fruits', 'Fruits and vegetables contain important vitamins, minerals and plant chemicals.', 1),
+(3, 'Bakery', 'Bakery products, which include bread, eggs, cookies, and muffins.', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `pdt_id` int(255) NOT NULL,
+  `pdt_name` varchar(200) NOT NULL,
+  `pdt_price` int(255) NOT NULL,
+  `pdt_des` varchar(255) NOT NULL,
+  `pdt_ctg` int(200) NOT NULL,
+  `pdt_img` varchar(255) NOT NULL,
+  `product_stock` int(5) NOT NULL,
+  `pdt_status` tinyint(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`pdt_id`, `pdt_name`, `pdt_price`, `pdt_des`, `pdt_ctg`, `pdt_img`, `product_stock`, `pdt_status`) VALUES
+(1, ' Lemon (Kg)', 5, 'Lemon (Citrus limon (L.) Burm.) a yellow or pale yellow prolate fruit. ', 2, 'p-07.jpg', 10, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `product_info_ctg`
+-- (See below for the actual view)
+--
+CREATE TABLE `product_info_ctg` (
+`pdt_id` int(255)
+,`pdt_name` varchar(200)
+,`pdt_price` int(255)
+,`pdt_des` varchar(255)
+,`pdt_img` varchar(255)
+,`product_stock` int(5)
+,`pdt_status` tinyint(5)
+,`ctg_id` int(11)
+,`ctg_name` varchar(60)
+);
 
 -- --------------------------------------------------------
 
@@ -61,6 +126,8 @@ CREATE TABLE `users` (
   `user_password` varchar(255) NOT NULL,
   `user_mobile` int(11) NOT NULL,
   `user_address` varchar(255) NOT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `reset_token_expire` date DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `modified_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -69,11 +136,23 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `user_name`, `user_firstname`, `user_lastname`, `user_email`, `user_password`, `user_mobile`, `user_address`, `created_at`, `modified_at`) VALUES
-(1, 'aa_khan', ' Arfan', ' A Khan', 'khan.arfan07@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 2147483647, '525 Kathleen St', '2023-10-27 13:47:33', '2023-10-27 13:47:33'),
-(2, 'test', ' test', ' case', 'test@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 1234561234, 'paris st', '2023-10-27 16:37:13', '2023-10-27 16:37:13'),
-(3, 'Team18', ' team', ' eighteen', 'team18@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 1231231234, 'laurentian university.', '2023-10-27 20:17:21', '2023-10-27 20:17:21'),
-(4, 'jira', ' jira', ' soft', 'jira@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 1231231234, '525 Kathleen St', '2023-10-27 22:28:22', '2023-10-27 22:28:22');
+INSERT INTO `users` (`user_id`, `user_name`, `user_firstname`, `user_lastname`, `user_email`, `user_password`, `user_mobile`, `user_address`, `reset_token`, `reset_token_expire`, `created_at`, `modified_at`) VALUES
+(1, 'aa_khan', ' Arfan', ' A Khan', 'khan.arfan07@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 2147483647, '525 Kathleen St', NULL, NULL, '2023-10-27 13:47:33', '2023-10-27 13:47:33'),
+(2, 'test', ' test', ' case', 'test@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 1234561234, 'paris st', NULL, NULL, '2023-10-27 16:37:13', '2023-10-27 16:37:13'),
+(3, 'Team18', ' team', ' eighteen', 'team18@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 1231231234, 'laurentian university.', NULL, NULL, '2023-10-27 20:17:21', '2023-10-27 20:17:21'),
+(4, 'jira', ' jira', ' soft', 'jira@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 1231231234, '525 Kathleen St', NULL, NULL, '2023-10-27 22:28:22', '2023-10-27 22:28:22'),
+(5, 'new', ' new', ' user', 'newuser@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 1234567896, 'lu', NULL, NULL, '2023-11-09 01:22:32', '2023-11-09 01:22:32'),
+(6, 'test22', ' test', ' 12', 'test22@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 1231231234, 'downtown', NULL, NULL, '2023-11-09 01:39:31', '2023-11-09 01:39:31'),
+(7, 'Arfan', ' Arfan', ' A Khan', 'alpharfan07@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 1231231234, 'Laurentian University.', '937f4365ecc4de3db67f9d6951d4caac', '2023-11-11', '2023-11-11 22:53:15', '2023-11-11 22:53:15');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `product_info_ctg`
+--
+DROP TABLE IF EXISTS `product_info_ctg`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `product_info_ctg`  AS SELECT `products`.`pdt_id` AS `pdt_id`, `products`.`pdt_name` AS `pdt_name`, `products`.`pdt_price` AS `pdt_price`, `products`.`pdt_des` AS `pdt_des`, `products`.`pdt_img` AS `pdt_img`, `products`.`product_stock` AS `product_stock`, `products`.`pdt_status` AS `pdt_status`, `catagory`.`ctg_id` AS `ctg_id`, `catagory`.`ctg_name` AS `ctg_name` FROM (`products` join `catagory`) WHERE `products`.`pdt_ctg` = `catagory`.`ctg_id` ;
 
 --
 -- Indexes for dumped tables
@@ -84,6 +163,18 @@ INSERT INTO `users` (`user_id`, `user_name`, `user_firstname`, `user_lastname`, 
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `catagory`
+--
+ALTER TABLE `catagory`
+  ADD PRIMARY KEY (`ctg_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`pdt_id`);
 
 --
 -- Indexes for table `users`
@@ -102,10 +193,22 @@ ALTER TABLE `admin`
   MODIFY `admin_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT for table `catagory`
+--
+ALTER TABLE `catagory`
+  MODIFY `ctg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `pdt_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
